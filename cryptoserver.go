59bd4f3e545f7c113d3ab59971bf9c45ca2282c3
St1lon <dima.musolin@gmail.com>
2026-01-26 22:30:29 +0300

    f

--- cryptoserver.go
+++ cryptoserver.go
@@ -13,7 +13,6 @@ func main() {
 	userRepo := repository.NewMemoryUserRepository()
 	mux.HandleFunc("POST /auth/register", handlers.HandlerRegister(userRepo))
 	mux.HandleFunc("POST /auth/login", handlers.HandlerAuth(userRepo))
-
 	log.Println("Server starting on :8080")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
