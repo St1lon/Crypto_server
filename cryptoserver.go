d80e40bad75da11619a62c0973ba92e286718c36
Dmitry <dima.musolin@gmail.com>
2026-02-08 05:40:13 +0300

    Rewrite main server startup to use new controller and adapter packages

--- cryptoserver.go
+++ cryptoserver.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	//"cryptoserver/domain"
-	"cryptoserver/internal/auth/handlers"
-	"cryptoserver/internal/repository"
+	"cryptoserver/internal/controller"
+	"cryptoserver/internal/adapter"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	userRepo := repository.NewMemoryUserRepository()
-	mux.HandleFunc("POST /auth/register", handlers.HandlerRegister(userRepo))
-	mux.HandleFunc("POST /auth/login", handlers.HandlerAuth(userRepo))
+	userRepo := adapter.NewMemoryUserRepository()
+	mux.HandleFunc("POST /auth/register", controller.HandlerRegister(userRepo))
+	mux.HandleFunc("POST /auth/login", controller.HandlerLogin(userRepo))
 	log.Println("Server starting on :8080")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
